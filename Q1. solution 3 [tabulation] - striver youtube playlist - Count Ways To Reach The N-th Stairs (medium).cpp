69c72034015f507d78a469314effde513dac57b0
chaitanyavankhande <66583050+chaitanyavankhande@users.noreply.github.com>
2023-06-10 00:28:08 +0530

    Update Q1. solution 3 [tabulation] - striver youtube playlist - Count Ways To Reach The N-th Stairs (medium).cpp

--- Q1. solution 3 [tabulation] - striver youtube playlist - Count Ways To Reach The N-th Stairs (medium).cpp
+++ Q1. solution 3 [tabulation] - striver youtube playlist - Count Ways To Reach The N-th Stairs (medium).cpp
@@ -4,7 +4,7 @@
 //TC: O(n)
 // SC: O(n)
 
-
+//method 1
 #include <bits/stdc++.h> 
 using namespace std;
 #define MOD int(1e9 + 7)
@@ -21,6 +21,26 @@ int countDistinctWays(int n) {
         else {
             tb[i] = tb[i-1] % MOD;
         }
+    }
+    return tb[n];
+}
+
+//method 2
+#include <bits/stdc++.h> 
+using namespace std;
+#define MOD int(1e9 + 7)
+
+//tabulation solution
+int countDistinctWays(int n) {
+  //  Write your code here.
+    if(n == 0) return 1;
+    if(n <= 2) return n;
+
+    vector<int>tb(n+1, 0);
+    tb[0] = 1;
+    tb[1] = 1;
+    for(int i=2; i<=n; i++) {
+        tb[i] = (tb[i-1] + tb[i-2]) % MOD;
     }
     return tb[n];
 }
