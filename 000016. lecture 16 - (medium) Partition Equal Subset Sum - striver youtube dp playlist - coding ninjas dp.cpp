39bcb5c1e9561432859b63b7dd9e8c550f4d7a60
chaitanyavankhande <66583050+chaitanyavankhande@users.noreply.github.com>
2023-07-12 18:57:08 +0530

    Update 000016. lecture 16 - (medium) Partition Equal Subset Sum - striver youtube dp playlist - coding ninjas dp.cpp

--- 000016. lecture 16 - (medium) Partition Equal Subset Sum - striver youtube dp playlist - coding ninjas dp.cpp
+++ 000016. lecture 16 - (medium) Partition Equal Subset Sum - striver youtube dp playlist - coding ninjas dp.cpp
@@ -1,4 +1,30 @@
 
+/* solution 3: memoization */
+bool solve(vector<int>&arr, int n, int target, vector<vector<int>>&dp) {
+	if(target == 0) return true;
+	if(n == 0) return false;
+	
+	if(dp[n][target] != -1) return dp[n][target];
+	bool pick = false;
+	if(target >= arr[n-1]) pick = solve(arr, n-1, target - arr[n-1], dp);
+	if(pick) return true;
+	
+	bool notPick = solve(arr, n-1, target, dp);
+	return dp[n][target] = notPick;
+}
+
+bool canPartition(vector<int> &arr, int n)
+{
+	// Write your code here.
+	int allSum = 0;
+	for(int i=0; i<n; i++) allSum += arr[i];
+	if(allSum % 2 == 1) return false;
+
+	int target = allSum / 2;
+	vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+	
+	return solve(arr, n, target, dp);
+}
 
 /* solution 4: recursion */
 bool solve(vector<int>&arr, int n, int target) {
