55e99a711c666907874e48594a22add18c55f1a1
chaitanyavankhande <66583050+chaitanyavankhande@users.noreply.github.com>
2023-07-14 23:53:23 +0530

    Update and rename 000016. (medium) 322. Coin Change - leetcode.cpp to 000019. (medium) 322. Coin Change - leetcode.cpp

--- 000019. (medium) 322. Coin Change - leetcode.cpp
+++ 000019. (medium) 322. Coin Change - leetcode.cpp
@@ -25,3 +25,26 @@ public:
         return ans;
     }
 };
+
+/* solution 4: recursion */
+class Solution {
+    int solve(vector<int>&coins, int amount, int n) {
+
+        if(amount == 0) return 0;
+        if(n == 0) return INT_MAX;
+        int pick = INT_MAX;
+        if(amount >= coins[n-1]) pick = solve(coins, amount - coins[n-1], n);
+        if(pick != INT_MAX) pick += 1;
+        int notPick = solve(coins, amount, n-1);
+        int ans = min(pick, notPick);
+        
+        return ans;
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int ans = solve(coins, amount, n);
+        if(ans == INT_MAX) ans = -1;
+        return ans;
+    }
+};
