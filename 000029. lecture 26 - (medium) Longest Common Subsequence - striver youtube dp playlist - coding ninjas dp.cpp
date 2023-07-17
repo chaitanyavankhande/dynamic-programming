a5fa7efcefa3fa940a73cc70a9828eba7cb0bee9
chaitanyavankhande <66583050+chaitanyavankhande@users.noreply.github.com>
2023-07-17 10:35:57 +0530

    Update 000029. lecture 26 - (medium) Longest Common Subsequence - striver youtube dp playlist - coding ninjas dp.cpp

--- 000029. lecture 26 - (medium) Longest Common Subsequence - striver youtube dp playlist - coding ninjas dp.cpp
+++ 000029. lecture 26 - (medium) Longest Common Subsequence - striver youtube dp playlist - coding ninjas dp.cpp
@@ -10,18 +10,22 @@ int getLengthOfLCS(string & str1, string & str2) {
   vector<int> previous(n2+1, 0), current(n2+1, 0);
   for(int i=1; i<=n1; i++) {
     for(int j=1; j<=n2; j++) {
-      int both = 0;
       if(str1[i-1] == str2[j-1]) {
+        int both = 0;
         both = 1 + previous[j-1];
+        current[j] = both;
       }
-      int first = current[j-1];
-      int second = previous[j];
-      current[j] = max(both, max(first, second));
+      else {
+        int first = current[j-1];
+        int second = previous[j];
+        current[j] = max(first, second);
+      }
     }
     previous = current;
   }
   return previous[n2];
 }
+
 
 /* solution 2: tabulation */
 int getLengthOfLCS(string & str1, string & str2) {
@@ -30,13 +34,16 @@ int getLengthOfLCS(string & str1, string & str2) {
   vector<vector<int>> tb(n1+1, vector<int>(n2+1, 0));
   for(int i=1; i<=n1; i++) {
     for(int j=1; j<=n2; j++) {
-      int both = 0;
       if(str1[i-1] == str2[j-1]) {
+        int both = 0;
         both = 1 + tb[i-1][j-1];
+        tb[i][j] = both;
       }
-      int first = tb[i][j-1];
-      int second = tb[i-1][j];
-      tb[i][j] = max(both, max(first, second));
+      else {
+        int first = tb[i][j-1];
+        int second = tb[i-1][j];
+        tb[i][j] = max(first, second);
+      }
     }
   }
   return tb[n1][n2];
@@ -51,6 +58,7 @@ int solve(string &str1, int n1, string &str2, int n2, vector<vector<int>>&dp) {
   int both = 0;
   if(str1[n1-1] == str2[n2-1]) {
     both = 1 + solve(str1, n1-1, str2, n2-1, dp);
+    return both;
   }
   int first = solve(str1, n1, str2, n2-1, dp);
   int second = solve(str1, n1-1, str2, n2, dp);
@@ -64,12 +72,14 @@ int getLengthOfLCS(string & str1, string & str2) {
 }
 
 
+
 /* solution 4: recrusion */
 int solve(string &str1, int n1, string &str2, int n2) {
   if(n1 == 0 || n2 == 0) return 0;
   int both = 0;
   if(str1[n1-1] == str2[n2-1]) {
     both = 1 + solve(str1, n1-1, str2, n2-1);
+    return both;
   }
   int first = solve(str1, n1, str2, n2-1);
   int second = solve(str1, n1-1, str2, n2);
@@ -79,4 +89,3 @@ int getLengthOfLCS(string & str1, string & str2) {
   // Write your code here.
   return solve(str1, str1.size(), str2, str2.size());
 }
-
